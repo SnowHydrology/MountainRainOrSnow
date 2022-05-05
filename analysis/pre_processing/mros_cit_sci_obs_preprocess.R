2ba4041021f924c120de6e1750715f559f543137
K. Jennings <32177682+SnowHydrology@users.noreply.github.com>
2022-05-05 10:14:33 -0400

    update preprocess for 2021-2022 data

--- analysis/pre_processing/mros_cit_sci_obs_preprocess.R
+++ analysis/pre_processing/mros_cit_sci_obs_preprocess.R
@@ -6,36 +6,63 @@
 
 # Load packages
 library(tidyverse)
+library(lubridate)
 library(cowplot); theme_set(theme_cowplot())
 library(sp)
 library(rgdal)
 library(raster)
+library(lutz) # time zone calculations
 
 ################################################################################
 ########################  Data Import and Preparation  #########################
 ################################################################################
+raw.file = "data/NOSHARE/mros_obs_raw_20220503.csv" # 2021-2022 data
+# 2020-2021 data: "data/NOSHARE/Tahoe__Rain_or_Snow_Export_06012021.csv"
+export.file = "data/NOSHARE/mros_obs_cit_sci_processed_20220503.RDS"
+export.shape.dsn = "data/geospatial/"
+export.shape.layer = "mros_obs_cit_sci_shp_20220503"
 
 # Import the citizen science data 
-# Add some date and time info
-obs <- read.csv("data/NOSHARE/Tahoe__Rain_or_Snow_Export_06012021.csv",
+obs <- read.csv(raw.file,
                 stringsAsFactors = F) %>% 
-  rename("phase" = "What_is_falling_from_the_sky_right_now._") %>% 
-  mutate(datetime = as.POSIXct(createdAt, 
-                               format = "%m/%d/%Y %H:%M",
-                               tz = "Etc/GMT+8"),
-         date = as.Date(datetime, tz = "Etc/GMT+8"),
-         hour = as.numeric(format(datetime, "%H")),
-         time = format(datetime, "%H:%M"),
-         day_of_week = weekdays(datetime))
+  rename("phase" = "precipitation") %>% 
+  mutate(timezone = tz_lookup_coords(lat = latitude, 
+                                     lon = longitude, 
+                                     method = "accurate")) %>% 
+  split(.$timezone)
 
-# Filter center-of-Tahoe obs
-# obs w/ no lat or lon
-# obs before 2020
-obs <- obs %>% 
-  filter(., latitude != 39.096800000000002 & 
-           longitude != -120.032399999999996) %>% 
-  filter(., !is.na(latitude) | !is.na(longitude)) %>% 
-  filter(., date >= "2020-01-01")
+# Calculate time info
+# Note this is more complex than just calculating a new column
+# Why? R no likey multiple time zones in one column
+# So we split out time zones into different list elements
+# Then compute local time info, convert to UTC and join everything back together
+obs <- lapply(obs, function(x){
+  data.frame(x) %>% 
+    mutate(datetime = with_tz(time = x$createdAt, 
+                              tzone = x[[1, "timezone"]]),
+           date = as.Date(datetime, tz = x[[1, "timezone"]]),
+           year = year(date),
+           month = month(date),
+           day = day(date),
+           hour = hour(datetime),
+           minute = minute(datetime),
+           second = second(datetime),
+           day_of_week = weekdays(datetime),
+           doy = yday(datetime),
+           dowy = ifelse(year %% 4 == 0,
+                         ifelse(month >= 10,
+                                doy - 274,
+                                doy + 92),
+                         ifelse(month >= 10,
+                                doy - 273,
+                                doy + 92)),
+           utc_datetime = with_tz(time = datetime, 
+                                  tzone = "UTC"), 
+           datetime = NULL)
+})
+
+# Bind all the data back together
+obs <- plyr::ldply(obs, bind_rows)
 
 # Add id
 obs$id <- 1:length(obs$phase)
@@ -47,7 +74,6 @@ eco <- readOGR(dsn = "data/geospatial/", layer = "us_eco_l4_no_st_4326") %>%
 # Import state data
 states <- readOGR(dsn = "data/geospatial/", layer = "USA_adm1_WGS84") %>% 
   spTransform(., CRS("+init=epsg:4326"))
-
 
 ################################################################################
 #########################  Data Joins and Extraction  ##########################
@@ -64,12 +90,6 @@ obs_sp <- SpatialPointsDataFrame(coords = coords,
                                  data = ids, 
                                  proj4string = crs_obs)
 
-# Add elevation data to obs based on location
-# Note: velox package is faster for extraction but time spent 
-# converting to velox object eliminates extraction gains here
-obs$elev <- raster("data/geospatial/elev_westUS_90m.tif") %>% 
-  extract(., obs_sp)
-
 # Use sp::over to perform spatial join of obs and eco & states data
 obs <- bind_cols(obs, 
                  sp::over(obs_sp, eco),
@@ -79,14 +99,28 @@ obs <- bind_cols(obs,
 #########################  Reformat and Export Data  ###########################
 ################################################################################
 
+# Filter out data outside the US and without a valid phase
+obs <- obs %>% 
+  filter(!is.na(US_L3NAME) & phase != " - ")
+obs_sp <- obs_sp[obs_sp$id %in% obs$id, ]
+
 # Select and rename relevant data
 obs <- obs %>% 
-  dplyr::select(., observer, latitude:elev,
+  dplyr::select(., id,
+                datetime_local_obs = createdAt,
+                datetime_local_sub = updatedAt,
+                observer:utc_datetime,
                 eco_l4 = US_L4NAME,
                 eco_l3 = US_L3NAME,
                 eco_l2 = NA_L2NAME,
                 state = NAME_1)
 
-# Export
+# Export the preprocessed dataset
 saveRDS(object = obs,
-        file = "data/NOSHARE/mros_cit_sci_obs_processed.RDS")
+        file = export.file)
+
+# Export the preprocessed shapefile
+writeOGR(obs_sp, 
+         dsn = export.shape.dsn, 
+         layer = export.shape.layer, 
+         driver = "ESRI Shapefile")

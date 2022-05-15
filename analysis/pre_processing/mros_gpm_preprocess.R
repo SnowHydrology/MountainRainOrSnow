3972b93411efd86992be9d73e40f0f7a662a73de
K. Jennings <32177682+SnowHydrology@users.noreply.github.com>
2022-05-15 13:41:06 -0400

    updated gpm processing

--- analysis/pre_processing/mros_gpm_preprocess.R
+++ analysis/pre_processing/mros_gpm_preprocess.R
@@ -4,11 +4,18 @@
 
 
 library(tidyverse)
-library(lubridate)
-library(cowplot); theme_set(theme_cowplot())
+
+########## User input
+
+# Input files
+citsci.input = "data/processed/mros_obs_processed_20220503.RDS"
+gpm.input = "data/NOSHARE/gpm_imerg_pts_20220503_wId.csv"
+
+# Output files
+gpm.output = "data/processed/mros_gpm_processed_20220503.RDS"
 
 # Import processed GPM data
-gpm <- read.csv("data/NOSHARE/mros_gpm_imerg_20210730.csv") %>% 
+gpm <- read.csv(gpm.input) %>% 
   rename(gpm_prob = probabilityLiquidPrecipitation) # renamed for easier typing
 
 # Format date information
@@ -18,22 +25,17 @@ gpm <- gpm %>%
                                tz = "UTC"))
 
 # Import the citizen science observations
-obs <- readRDS("data/processed/mros_obs_processed_2020_2021.RDS") %>% 
-  filter(tair_flag == "Pass" & 
-           ppt_flag == "Pass" & 
-           rh_flag == "Pass" &
-           dist_flag == "Pass" & 
-           dupe_flag == "Pass")
+obs <- readRDS(citsci.input)
 
 # Join gpm data with obs by id
 gpm_obs <- left_join(select(gpm, id, gpm_prob, datetime),
-                     select(obs, id, datetime, phase, tair),
+                     select(obs, id, utc_datetime, phase, tair),
                      by = "id")
 
 # Compute difference in time between gpm and obs
 # Then get minimum time diff per obs
 gpm_obs <- gpm_obs %>% 
-  mutate(time_diff_abs = abs(datetime.x - datetime.y)) %>% 
+  mutate(time_diff_abs = abs(datetime - utc_datetime)) %>% 
   group_by(id) %>% 
   mutate(time_diff_min = min(time_diff_abs, na.rm = T))
 
@@ -48,5 +50,5 @@ gpm_obs <- gpm_obs %>%
 
 # Export
 saveRDS(object = gpm_obs,
-        file = "data/processed/mros_gpm_processed_2020_2021.RDS")
+        file = gpm.output)
 

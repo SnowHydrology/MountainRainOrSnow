a11bbf72fdc26b8b36b20bff01d3f6a39dd236d5
K. Jennings <32177682+SnowHydrology@users.noreply.github.com>
2022-06-13 08:54:22 -0400

    rain-snow elevation update by ecoregion/day

--- analysis/rain_snow/mros_obs_rain_snow_line.R
+++ analysis/rain_snow/mros_obs_rain_snow_line.R
@@ -1,10 +1,5 @@
 # Mountain rain or snow rain-snow line creation from citsci data
 
-
-# Keith Jennings
-# 2021-12-06
-# ksj.hydro@gmail.com
-
 # Load packages
 library(tidyverse)
 library(lubridate)
@@ -12,33 +7,54 @@ library(cowplot); theme_set(theme_cowplot())
 library(foreach) # for parallel processing
 library(doMC); registerDoMC(cores = 4)
 
+
+########## User input
+
+# Input files
+citsci.input = "data/processed/mros_obs_processed_20220503.RDS"
+
+# Threshold for n valid obs per grouping unit
+obs_thresh = 300
+
+# Compute the state and ecoregion number of valid obs
+eco3 <- obs %>% group_by(eco_l3) %>% summarise(n = n()) %>% 
+  filter(n > obs_thresh)
+eco4 <- obs %>% group_by(eco_l4) %>% summarise(n = n()) %>% 
+  filter(n > obs_thresh) %>% 
+  filter(eco_l4 != "Sedimentary Subalpine Forests")
+state <- obs %>% group_by(state) %>% summarise(n = n()) %>% 
+  filter(n > obs_thresh)
+
 # Import obs
-obs <- readRDS("data/processed/mros_obs_processed_2020_2021.RDS") %>% 
+obs <- readRDS(citsci.input) %>% 
   filter(tair_flag == "Pass" & 
            ppt_flag == "Pass" & 
            rh_flag == "Pass" &
            dist_flag == "Pass" & 
-           dupe_flag == "Pass")
+           closest_flag == "Pass" &
+           nstation_flag == "Pass" &
+           eco_l3 %in% eco3$eco_l3)
 
 # Create elevation bins and names
 obs <- obs %>%
-  mutate(elev_bin2 = cut_width(elev, width = 250))
+  mutate(elev_bin = cut_width(elev, width = 100))
 
 obs_by_elev_date <- obs %>% 
-  group_by(date, elev_bin2) %>% 
+  group_by(date, elev_bin, eco_l3) %>% 
   summarise(snow_prob = sum(phase == "Snow")/length(phase) * 100) %>% 
-  mutate(elev = case_when(elev_bin2 == "[875,1.12e+03]" ~ 1000,
-                          TRUE ~ as.numeric(substr(elev_bin2, 2, 5)) * 1000 + 125))
+  mutate(elev = str_extract(string = elev_bin, pattern = "(?<=()).*(?=,)"),
+         elev = as.numeric(str_sub(elev, 2, -1)) + 50)
 
-# Get unique dates
-dates <- unique(obs_by_elev_date$date)
+# Get unique dates and locations
+dates_locs <- unique(obs_by_elev_date[c("date", "eco_l3")])
 
 # Compute 50% rain-snow elevation per scene
 rain_snow_line.l <-
-  foreach(i = 1:length(dates), .errorhandling = "pass") %dopar% {
+  foreach(i = 1:length(dates_locs$date), .errorhandling = "pass") %dopar% {
     
     # Filter GPM to datetime
-    tmp.df <- filter(obs_by_elev_date, date == dates[i])
+    tmp.df <- filter(obs_by_elev_date, date == pull(dates_locs[i, "date"]) &
+                       eco_l3 == pull(dates_locs[i, "eco_l3"]))
     
     # Check that there are probabilities above and below 50%
     if(min(tmp.df$snow_prob) >= 50){
@@ -59,7 +75,8 @@ rain_snow_line.l <-
     }
     
     # Output into dataframe
-    tmp.rain_snow_line <- data.frame(date = dates[i],
+    tmp.rain_snow_line <- data.frame(date =  pull(dates_locs[i, "date"]),
+                                     eco_l3 =  pull(dates_locs[i, "eco_l3"]),
                                      rs_line = tmp.rs_line,
                                      note = tmp.note)
     
@@ -78,6 +95,6 @@ saveRDS(object = rain_snow_line,
 
 
 # Plot all probs by elev bin
-ggplot(obs_by_elev_date, aes(elev_bin2, snow_prob)) + 
+ggplot(obs_by_elev_date, aes(elev_bin, snow_prob)) + 
   geom_point() + 
   facet_wrap(~date)

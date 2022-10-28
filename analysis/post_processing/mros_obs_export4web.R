b1889d6b15460eb7796fa516dc4ae73ac57221bf
K. Jennings <32177682+SnowHydrology@users.noreply.github.com>
2022-10-28 14:10:37 -0400

    join and export for web

---
+++ analysis/post_processing/mros_obs_export4web.R
@@ -0,0 +1,19 @@
+# Script for outputting processed citizen science data and ancillary info
+
+# TODO: move other post-processing steps here (currently buried in other files)
+
+# Load package
+library(tidyverse)
+
+# User input
+obs.file = "data/processed/mros_obs_processed_2020_2021.RDS"
+gpm.file = "data/processed/mros_gpm_processed_2020_2021.RDS"
+output.file = "data/processed/mros_obs_processed_2020_2021.csv"
+
+# Read in RDS files and join
+df <- readRDS(obs.file) %>% 
+  left_join(readRDS(gpm.file), by = "id") %>% 
+  select(-time_diff)
+
+# Export csv
+write.csv(df, output.file, quote = F, row.names = F)

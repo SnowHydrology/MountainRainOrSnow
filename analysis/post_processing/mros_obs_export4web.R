2726d310cd88d5c274153a9cd97a224cee46a9c9
K. Jennings <32177682+SnowHydrology@users.noreply.github.com>
2022-11-01 08:45:15 -0400

    updates for manuscript

--- analysis/post_processing/mros_obs_export4web.R
+++ analysis/post_processing/mros_obs_export4web.R
@@ -13,7 +13,8 @@ output.file = "data/processed/mros_obs_processed_2020_2021.csv"
 # Read in RDS files and join
 df <- readRDS(obs.file) %>% 
   left_join(readRDS(gpm.file), by = "id") %>% 
-  select(-time_diff)
+  select(-time_diff) %>% 
+  mutate(gpm_prob = round(gpm_prob, digits = 0))
 
 # Export csv
 write.csv(df, output.file, quote = F, row.names = F)
